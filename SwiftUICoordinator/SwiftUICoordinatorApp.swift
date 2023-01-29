2f0b1bf63b0be85d9eaf16c64823b5f39c03306b
rebeloper <therebeloper@gmail.com>
2023-01-29 11:41:45 +0200

    Initial Commit

---
+++ SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by Alex Nagy on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
